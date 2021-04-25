e2f22a36748476ef1f170f34470bb1c9ee47e5dd
MichalJaracz <61659012+MichalJaracz@users.noreply.github.com>
2021-04-25 18:43:12 +0200

    asd

--- server/src/service/mongoose.js
+++ server/src/service/mongoose.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 
-const uri = "mongodb+srv://michalj0721@gmail.com:Hokejt009@cluster0.73pmi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://michalj0721@gmail.com:Jakistam123@cluster0.73pmi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const mongoClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
