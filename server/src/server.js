5c54f484caac1664943177636a91f5e127f4b5ed
MichalJaracz <61659012+MichalJaracz@users.noreply.github.com>
2021-04-28 00:08:49 +0200

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -5,28 +5,102 @@ const bodyParser = require('body-parser');
 const { mongoClient } = require('./service/mongoose');
 const { Room } = require('./schema/room');
 
+const createRoom = async () => {
+    return new Room({
+        gameOn: false,
+        plansza: {
+            red: [0, 1, 2, 3],
+            green: [0, 1, 2, 3],
+            blue: [0, 1, 2, 3],
+            yellow: [0, 1, 2, 3],
+        },
+        red: {
+            nick: 'Jan',
+            insertTime: Date.now(),
+            lastAct: Date.now(),
+            serverTime: Date.now(),
+            status: 'void',
+        },
+        green: {
+            nick: 'Ela',
+            insertTime: Date.now(),
+            lastAct: Date.now(),
+            serverTime: Date.now(),
+            status: 'void',
+        },
+        blue: {
+            nick: 'Joe',
+            insertTime: Date.now(),
+            lastAct: Date.now(),
+            serverTime: Date.now(),
+            status: 'void',
+        },
+        yellow: {
+            nick: 'Dupa',
+            insertTime: Date.now(),
+            lastAct: Date.now(),
+            serverTime: Date.now(),
+            status: 'void',
+        },
+    });
+};
+
+const getLastRoom = async () => {
+    const rooms = await Room.find();
+    return rooms.pop();
+};
+
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000000 } }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 mongoClient.then(async err => {
-    app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 } }));
 
-    app.use(bodyParser.urlencoded({ extended: true }));
+    app.get('/create-room', async function (req, res, next) {
+        const room = createRoom();
+        await room.save();
 
-    app.get('/', function (req, res, next) {
-        const room = new Room({ id: Date.now() });
+        res.end();
+    });
 
-        room.save(function (err, fluffy) {
-            if (err) return console.error(err);
-            return;
-        });
-        // if (req.session.views) {
-        //     req.session.views++
-        //     res.setHeader('Content-Type', 'text/html')
-        //     res.write('<p>views: ' + req.session.views + '</p>')
-        //     res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
-        //     res.end()
-        // } else {
-        //     req.session.views = 1
-        //     res.end('welcome to the session demo. refresh!')
-        // }
+    app.get('/username', async function (req, res, next) {
+        if (req.query.name) {
+            req.session.username = req.query.name;
+
+            res.end();
+        }
+        res.end('Set username!!!!! /username?name=RandomName');
+    });
+
+    app.get('/room', async function (req, res, next) {
+        let responseData = null;
+
+        // sprawdz czy username jest ustawione w sesji
+        // jezeli nie to zwroci null i wyswietl komponent do wprowadzenia nicku
+        if (req.session.username) {
+            if (req.session.userRoomId) {
+                responseData = await Room.findById(req.session.userRoomId);
+            }
+
+            if (!req.session.userRoomId) {
+                const lastRoom = await getLastRoom();
+
+                if (lastRoom.gameOn) {
+                    const newRoom = await createRoom();
+                    await newRoom.save();
+                    responseData = newRoom;
+
+                    req.session.userRoomId = newRoom._id;
+                }
+
+                if (!lastRoom.gameOn) {
+                    req.session.userRoomId = lastRoom._id;
+                    responseData = await Room.findById(req.session.userRoomId);
+                }
+            }
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.send(responseData);
     });
 
     app.listen(3000);
