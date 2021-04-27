a5f37f5833baa6548dd87cd6288c76fea96e34f5
MichalJaracz <61659012+MichalJaracz@users.noreply.github.com>
2021-04-28 00:07:32 +0200

    Update room.js

--- server/src/schema/room.js
+++ server/src/schema/room.js
@@ -1,11 +1,64 @@
 const mongoose = require('mongoose');
 
 const roomSchema = new mongoose.Schema({
-  id: Number,
-  started: Boolean,
-  plansza: Array,
+  gameOn: Boolean,
+  plansza: {
+    red: [Number],
+    green: [Number],
+    blue: [Number],
+    yellow: [Number],
+  },
+  red: {
+    nick: String,
+    color: String,
+    insertTime: Number,
+    lastAct: Number,
+    serverTime: Number,
+    status: {
+      type: String,
+      enum: ['connected', 'ready', 'void'],
+      default: 'void',
+    },
+  },
+  green: {
+    nick: String,
+    color: String,
+    insertTime: Number,
+    lastAct: Number,
+    serverTime: Number,
+    status: {
+      type: String,
+      enum: ['connected', 'ready', 'void'],
+      default: 'void',
+    },
+  },
+  blue: {
+    nick: String,
+    color: String,
+    insertTime: Number,
+    lastAct: Number,
+    serverTime: Number,
+    status: {
+      type: String,
+      enum: ['connected', 'ready', 'void'],
+      default: 'void',
+    },
+  },
+  yellow: {
+    nick: String,
+    color: String,
+    insertTime: Number,
+    lastAct: Number,
+    serverTime: Number,
+    status: {
+      type: String,
+      enum: ['connected', 'ready', 'void'],
+      default: 'void',
+    },
+  },
 });
 
 const Room = mongoose.model('Room', roomSchema);
 
 module.exports = { Room };
+
